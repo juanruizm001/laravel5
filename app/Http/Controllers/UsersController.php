bccc9bb8e38f87ad12a24ac05a6ee276808bc975
Juan Ruiz <juanruizm001@gmail.com>
2015-05-30 14:30:36 -0300

    seleccionamos los campos a ser devueltos y damos formato al JSON

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
-class UsersController extends Controller {
+class UsersController extends Controller
+{
 
     public function getIndex()
     {
-        $result = \DB::table('users')->get();
+        $result = \DB::table('users')
+            ->select(['users.first_name', 'users.last_name']) //Seleccionamos los campos que queremos que sean devueltos
+            ->get();
 
+        dd($result); //Damos formato más amigable al resultado JSON devuelto
 
         return $result;
     }
